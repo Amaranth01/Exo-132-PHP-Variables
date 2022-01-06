f387ed544b3ab64c8d962bbe614ad482c0a18e3b
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-06 15:32:21 +0100

    Exo 132 ok

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Exo 131</title>
+</head>
+<body>
+
+<?php
+
+$texte = "Du texte est stocké";
+echo $texte . "<br>";
+
+$y = 30;
+echo "y vaux $y" . "<br>";
+
+$m = 5;
+$n = 7;
+echo "$m + $n";
+
+?>
+
+<script src="https://kit.fontawesome.com/5a3255fd1d.js" crossorigin="anonymous"></script>
+</body>
+</html>
